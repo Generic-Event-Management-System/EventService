a5a6c269cd955418888d54fe61d3cc8e41da1bdd
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-01 23:05:14 +0200

    Added Event entity in the project.

---
+++ EventService/Models/Entities/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace EventService.Models.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required decimal Fee { get; set; }
+        public required string Venue { get; set; }
+        public required DateTime EventDate { get; set; }
+        public required int MaxTeamsAllowed { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
