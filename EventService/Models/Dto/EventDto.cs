ad615afd5e01493c61c88caf564aa6ea0b9f235e
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-01 23:24:20 +0200

    Created EventDto class.

---
+++ EventService/Models/Dto/EventDto.cs
@@ -0,0 +1,12 @@
+﻿namespace EventService.Models.Dto
+{
+    public class EventDto
+    {
+        public required string Name { get; set; }
+        public required decimal Fee { get; set; }
+        public required string Venue { get; set; }
+        public required DateTime EventDate { get; set; }
+        public required int MaxTeamsAllowed { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
